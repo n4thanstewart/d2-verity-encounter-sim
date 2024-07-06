9e1af9c9969bf97b583987725037170adc72f6e3
Nathan Stewart <95386143+n4thanstewart@users.noreply.github.com>
2024-07-06 09:23:12 +0100

    Update verity-dissection-sim.py

--- verity-dissection-sim.py
+++ verity-dissection-sim.py
@@ -3,6 +3,7 @@ import time
 import threading
 import sys
 
+cr=True
 shapes = {"C": "circle", "S": "square", "T": "triangle"}
 
 def combineShapes(shape_pair):
@@ -33,30 +34,23 @@ def setupOuterStatues():
     rightStatue = all_shapes[4:]
     return leftStatue, middleStatue, rightStatue
 
-def setupEncounter(challengeRating):
+def setupEncounter(challengeType):
     callout = generateInnerCallout()
-    print(f"\nInside callout: {callout}\n")
-    if challengeRating=='':
+    if challengeType=='' or challengeType is None:
         leftStatue, middleStatue, rightStatue = setupOuterStatues()
-    elif challengeRating=="1":
+    elif challengeType=="1":
         def containsOneDouble(statue):
             return "Sphere" == combineShapes(statue) or "Cube" == combineShapes(statue) or "Pyramid" == combineShapes(statue)
         leftStatue, middleStatue, rightStatue = setupOuterStatues()
         while not containsOneDouble(leftStatue) and not containsOneDouble(middleStatue) and not containsOneDouble(rightStatue):
             leftStatue, middleStatue, rightStatue = setupOuterStatues()
             break
-    elif challengeRating=="2":
+    elif challengeType=="2":
         shapes = ["circle", "triangle", "square"]
         random.shuffle(shapes)
         leftStatue = [shapes[0],shapes[0]]
         middleStatue = [shapes[1],shapes[1]]
         rightStatue = [shapes[2],shapes[2]]
-    leftCombination = combineShapes(leftStatue)
-    middleCombination = combineShapes(middleStatue)
-    rightCombination = combineShapes(rightStatue)
-    print(f"Left Statue: {leftCombination}")
-    print(f"Middle Statue: {middleCombination}")
-    print(f"Right Statue: {rightCombination}\n")
     return callout, leftStatue, middleStatue, rightStatue
 
 def checkWinCondition(callout, leftStatue, middleStatue, rightStatue):
@@ -104,12 +98,25 @@ def startTimer(duration):
     return True
 
 def main():
-    challengeRating=input("Challenge rating: ")
-    callout, leftStatue, middleStatue, rightStatue = setupEncounter(challengeRating)
-    while checkWinCondition(callout, leftStatue, middleStatue, rightStatue):
-        callout, leftStatue, middleStatue, rightStatue = setupEncounter(challengeRating)
+    if cr:
+        challengeType=input("\nChallenge type [Hit Enter: Normal] [1: One Double] [2: All Doubles]: ")
+    else:
+        challengeType=None
+    
+    callout, leftStatue, middleStatue, rightStatue = setupEncounter(challengeType)
 
-    timer_thread = threading.Thread(target=startTimer, args=(90,))
+    while checkWinCondition(callout, leftStatue, middleStatue, rightStatue):
+        callout, leftStatue, middleStatue, rightStatue = setupEncounter(challengeType)
+
+    print(f"\nInside callout: {callout}\n")
+    leftCombination = combineShapes(leftStatue)
+    middleCombination = combineShapes(middleStatue)
+    rightCombination = combineShapes(rightStatue)
+    print(f"Left Statue: {leftCombination}")
+    print(f"Middle Statue: {middleCombination}")
+    print(f"Right Statue: {rightCombination}\n")
+
+    timer_thread = threading.Thread(target=startTimer, args=(90,)) # 1 minute 30 seconds
     timer_thread.daemon = True
     timer_thread.start()
     
